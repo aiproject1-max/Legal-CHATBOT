5d8af1d5d49edb5291c51ec7f26c6f24b92d7233
aiproject1-max <ai.project.1.2025.4.19@gmail.com>
2025-05-10 14:52:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ groq_api_key = os.getenv("GROQ_API_KEY")
 # Streamlit UI setup
 st.set_page_config(page_title="LawGPT")
 col1, col2, col3 = st.columns([1, 4, 1])
-st.title("TT Legal ChatBot")
+st.title("TX Legal ChatBot")
 st.markdown("""
     <style>
     div.stButton > button:first-child {
